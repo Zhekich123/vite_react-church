1d9a283bec64e670b09c7cafe555098ffef20c93
Zhekich123 <axelb380@gmail.com>
2023-11-17 14:40:01 -0800

    prepare to map

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,5 @@
 import './Services.css';
 import CloseButton from '../../assets/images/close-button-dark.svg';
-import services from './Services_list.json';
 
 export const Services = () => {
 
@@ -16,58 +15,52 @@ export const Services = () => {
                             alt="modal-image"
                             id="modalImage"
                         />
-                        <img src={CloseButton}
-                            className="modal-close-btn"
-                            alt="close"
-                        />
-                        <div className="modalText">
-                            <div id="modalMainTitle"></div>
-                            <div id="modalMainText"></div>
-                            <div id="modalTitle"></div>
-                            <div id="modalTitleDescription"></div>
-                            <div id="modalText"></div>
-                            <div id="modalText2"></div>
-                            <div id="modalText3"></div>
-                            <div id="modalText4"></div>
-                            <div id="modalText5"></div>
-                            <div id="modalText6"></div>
-                            <div id="modalText7"></div>
-                        </div>
+                            <img src={CloseButton}
+                                className="modal-close-btn"
+                                alt="close"
+                            />
+                                <div className="modalText">
+                                    <div id="modalMainTitle"></div>
+                                    <div id="modalMainText"></div>
+                                    <div id="modalTitle"></div>
+                                    <div id="modalText"></div>
+                                    <div id="modalText2"></div>
+                                    <div id="modalText3"></div>
+                                    <div id="modalText4"></div>
+                                    <div id="modalText5"></div>
+                                    <div id="modalText6"></div>
+                                    <div id="modalText7"></div>
+                                </div>
+                            </div>
                     </div>
-                </div>
-                {services.map(service => (
-                    <div key={service.id} className={`services-list ${service.list}`}>
-                        <div className={`service-item ${service.item}`}>
+                    <div className="services-list services-list--1">
+                        <div className="service-item service-item--1">
                             <div className="service-item__info">
                                 <div className="service-item__top">
-                                    <div className={`service-item__title ${service.item_title}`}>{service.title}</div>
+                                    <div className="service-item__title service-item__title--1">Дитяче</div>
                                 </div>
 
-                                <div className={`service-item__description ${service.item_description}`}>
-                                    {service.description_title}
+                                <div className="service-item__description service-item__description--1">Ми любимо дітей і хочемо
+                                    служити дітям у нашій церкві та місті.
                                 </div>
 
-                                <a
-                                    href=""
-                                    className="service-item__link"
-                                    data-modal-description={service.description_modal}
-                                    data-modal-title={service.modal_title}
-                                    data-modal-text={service.modal_text}
-                                    data-modal-text2={service.modal_text2}
-                                    data-modal-text3={service.modal_text3}
-                                    data-modal-text4={service.modal_text4}
-                                    data-modal-text5={service.modal_text5}
-                                    data-modal-text6={service.modal_text6}
-                                    data-modal-image={service.image}
-                                >
-                                    Дізнатися більше
-                                </a>
+                                <a href="" className="service-item__link"
+                                    data-modal-title="Недільна школа – це місце:  "
+                                    data-modal-text="✓ де вивчають Біблію в доступній для дітей формі;"
+                                    data-modal-text2="✓ де розповідають основи християнської віри та біблійні сюжети.  
+              Основним і єдиним підручником в недільній школі є БІБЛІЯ.  Під час уроків діти слухають 
+              біблійні історії,співають пісні, вивчають вірші з Біблії, займаються рукоділлям."
+                                    data-modal-text3="Мета недільної школи:"
+                                    data-modal-text4="✓ запалити Божий вогонь в дитячих серцях;"
+                                    data-modal-text5="✓ навчити дітей любити Бога і один одного;"
+                                    data-modal-text6="✓ виховати майбутнє покоління на основі християнських цінностей!
+              Проходить кожної неділі о 10:00."
+
+                                    data-modal-image="./src/styles/images/services/1-kids-min.jpg">Дізнатися більше </a>
                             </div>
                         </div>
                     </div>
-                ))}
-
-            </div>
+                </div>
         </section>
     )
 }
